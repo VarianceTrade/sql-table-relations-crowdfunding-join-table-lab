ef52a538c7f3919a29bf6a4f7a4c8fb6ab7af61d
Brandon <sugb73@gmail.com>
2021-03-20 19:54:01 -0400

    commit tree 38a6ae05c50812321b07b2b289051f21602419d7

---
+++ lib/insert.sql
@@ -0,0 +1,147 @@
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Project 1", "Category A", 2000, "03/19/21", "03/20/21");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, "Project 2", "Category B", 3000, "03/120/21", "03/21/21");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (3, "Project 1", "Category A", 2000, "03/19/21", "03/20/21");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (4, "Project 1", "Category A", 2000, "03/19/21", "03/20/21");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (5, "Project 2", "Category B", 3000, "03/120/21", "03/21/21");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (6, "Project 1", "Category A", 2000, "03/19/21", "03/20/21");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (7, "Project 1", "Category A", 2000, "03/19/21", "03/20/21");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (8, "Project 2", "Category B", 3000, "03/120/21", "03/21/21");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (10, "Project 1", "Category A", 2000, "03/19/21", "03/20/21");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (11, "Project 1", "Category A", 2000, "03/19/21", "03/20/21");
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO users (name, age) VALUES ("John", 2);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50.00, 1, 1);
+
+
+
+
+
+
+
+
+
+
+
+
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr Chocolate", 5, "M", "brown", "cool", 1);
+
+
+
+-- CREATE TABLE users (
+--     id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     age INTEGER
+
+
+-- CREATE TABLE pledges (
+--     id INTEGER PRIMARY KEY,
+--     amount REAL,
+--     user_id INTEGER,
+--     project_id INTEGER
+-- );
